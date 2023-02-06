e3e96faba84f1e16abdb3fbedba95f79dd06f379
Harish Menon <b00087415@aus.edu>
2023-02-06 17:39:19 +0400

    Feature B Added!

--- src/lab1/intro.java
+++ src/lab1/intro.java
@@ -4,5 +4,11 @@ public class intro {
 
 	public static void main(String Args[]) {
 	System.out.println("Hello world");
+	featureB();
+	}
+	
+	public static void featureB()
+	{
+		System.out.println("Feature B");
 	}
 }
